b93606daa4d763494fa5cce38b0ac87109aab061
Gavin Giles <ggiles@gilescpa.com>
2023-05-03 23:02:33 -0700

    i still have to work on it.

--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -2,8 +2,9 @@ using System;
 
 public class job
 {
-    public string _company;
     public string _jobTitle;
+    
+    public string _company;
     public string _startYear;
     public string _endYear;
 
